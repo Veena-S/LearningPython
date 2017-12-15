8f840e1bd5ecbb331d314cb9a7080facbb12d2a8
Veena S <veenasivan@gmail.com>
2017-12-15 13:01:47 +0800

    Add files via upload

---
+++ 4_UsingExec/script1.py
@@ -0,0 +1,7 @@
+# First Python Script - Fetches the name of the platform.
+import sys              # Load a library module
+print( sys.platform )
+print( 2 ** 100 )       # Raise 2 to a power
+x = 'Spam!'
+print( x * 8 )          # String repetition.
+#input()                 # ADDED so that console window is not clicked while the file is executed by icon click.
