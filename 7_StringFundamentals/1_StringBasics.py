bbdcd4667ca2f52963b2cba9dc07abb4a1ad4dcd
Veena S <veenasivan@gmail.com>
2018-01-10 14:58:44 +0800

    Create 1_StringBasics.py

---
+++ 7_StringFundamentals/1_StringBasics.py
@@ -0,0 +1,2 @@
+#### Python strings are categorized as immutable sequences,
+#### meaning that the characters they contain have a left-to-right positional order and that they cannot be changed in place.
