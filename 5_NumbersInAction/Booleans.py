28e466cb611e1e1080a63b34b72af4e8779f21d5
Veena S <veenasivan@gmail.com>
2018-01-10 13:17:57 +0800

    Add files via upload

---
+++ 5_NumbersInAction/Booleans.py
@@ -0,0 +1,16 @@
+### BOOLEANS
+
+
+type( True )
+# Resule:   <class 'bool'>
+isinstance( True, int)
+# Resule:   True
+True == 1
+# Resule:   True
+# True has same value as 1, but different object
+True is 1
+# Resule:   False
+True or False
+# Resule:   True
+True + 4
+# Resule:   5
